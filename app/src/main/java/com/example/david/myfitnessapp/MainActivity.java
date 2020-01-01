dde5a5f838f45d8a7a6482f4426eea623b858ace
David Chung <david23@my.yorku.ca>
2020-01-01 13:03:47 -0500

    Found and fixed a bug in the login page

--- app/src/main/java/com/example/david/myfitnessapp/MainActivity.java
+++ app/src/main/java/com/example/david/myfitnessapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,8 +46,12 @@ public class MainActivity extends AppCompatActivity {
         String username = userView.getText().toString();
         EditText passView = (EditText) findViewById(R.id.pass);
         String password = passView.getText().toString();
-
-        validate (username, password);
+        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
+            validate(username, password);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Please fill out all the fields",Toast.LENGTH_SHORT).show();
+        }
 
 
 
